d0e3034e646ab06ecfd67a8a24443817529d638d
Mel Dube <mdube@ntserv.doitbestcorp.com>
2019-08-06 04:02:03 -0400

    continued file setup

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 // dependencies
 var express = require("express");
 var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8000;
+
+app.use(express.static("app/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+  console.log("I can here you on PORT: " + PORT);
+});
