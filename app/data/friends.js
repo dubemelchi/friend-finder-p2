99327cac2c864d089e2fd094dfc5c46a4724fe9e
dubemelchi <dubemelchi@gmail.com>
2019-08-06 08:41:20 -0400

    images of potentials

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,46 +1,46 @@
 const friends = [{
   "name": "Scarlet Johansen",
-  "photo": "",
+  "photo": "https://www.thepeninsulaqatar.com/uploads/2019/05/20/post_main_cover_fit//609fffaae4187728989593ecadc66394a8ad9c44.jpg",
   "scores":[5,3,5,1,1,1,2,3,1,3]
 },
 {
-  "name": "Scarlet Johansen",
-  "photo": "",
+  "name": "Emma Stone",
+  "photo": "https://media.self.com/photos/57d899d8f71ce8751f6b4edc/master/w_728,c_limit/GettyImages-457075950-1.jpg",
   "scores":[5,5,5,3,3,3,2,2,2,1]
 },
 {
-  "name": "Scarlet Johansen",
-  "photo": "",
+  "name": "Emily Blunt",
+  "photo": "https://www.cheatsheet.com/wp-content/uploads/2017/03/British-actress-Emily-Blunt.jpg",
   "scores":[1,3,5,5,3,1,3,3,1,3]
 },
 {
-  "name": "Scarlet Johansen",
-  "photo": "",
+  "name": "Margot Robbie",
+  "photo": "https://i.pinimg.com/originals/ab/00/44/ab004421731bc9f590218255d60ebf3c.jpg",
   "scores":[3,5,2,4,4,1,5,3,3,5]
 },
 {
-  "name": "Scarlet Johansen",
-  "photo": "",
+  "name": "Talulah Riley",
+  "photo": "https://i.dailymail.co.uk/i/pix/2016/07/27/11/3692E86000000578-3710410-Talulah_wears_DRESS_Peggy_Hartanto_EARRINGS_Tracey_Emin_for_Step-a-64_1469614409058.jpg",
   "scores":[1,3,2,3,5,5,1,3,4,1]
 },
 {
-  "name": "Scarlet Johansen",
-  "photo": "",
+  "name": "Jessica Alba",
+  "photo": "https://media.workandmoney.com/56/db/56db47ed4baa4c78949f9028f59b247d.jpg",
   "scores":[5,1,4,1,2,1,3,3,4,5,4]
 },
 {
-  "name": "Scarlet Johansen",
-  "photo": "",
+  "name": "Blake Lively",
+  "photo": "https://www.biography.com/.image/t_share/MTQyMDA0NzU2ODQyMjkyMjg2/gettyimages-578545760jpg.jpg",
   "scores":[5,3,1,2,2,2,1,5,2,3]
 },
 {
-  "name": "Scarlet Johansen",
-  "photo": "",
+  "name": "Olivia Wilde",
+  "photo": "https://media1.s-nbcnews.com/j/newscms/2017_06/1193702/olivia-wilde-2003-october-inline-today-170208_9925e9e389a182942a1112e474142649.fit-760w.jpg",
   "scores":[3,1,1,5,2,1,2,5,4,5]
 },
 {
-  "name": "Scarlet Johansen",
-  "photo": "",
+  "name": "Lisa Edelstein",
+  "photo": "https://i.pinimg.com/originals/5e/81/ec/5e81ecfd64244b729abbf6aaad0556ab.jpg",
   "scores":[3,1,1,5,2,1,2,5,4,5]
 },
 ];
